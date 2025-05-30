069c68838eeab62fc081e61e7e47e2565cfdd6eb
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-05-30 22:00:08 +0400

    add Rings in to the hero component

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -6,6 +6,16 @@ import { useState, useRef, useEffect } from "react";
 function Hero() {
   const [mounted, setMounted] = useState(false);
   const parallaxRef = useRef(null);
+  const Rings = () => {
+    return (
+      <>
+        <div className="absolute top-1/2 left-1/2 w-[65.875rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2 text-gray-400 opacity-10" />
+        <div className="absolute top-1/2 left-1/2 w-[51.375rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2 text-gray-400 opacity-10" />
+        <div className="absolute top-1/2 left-1/2 w-[36.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2 text-gray-400 opacity-10" />
+        <div className="absolute top-1/2 left-1/2 w-[23.125rem] aspect-square border border-n-2/10 rounded-full -translate-x-1/2 -translate-y-1/2 text-gray-400 opacity-10" />
+      </>
+    );
+  };
 
   useEffect(() => {
     setMounted(true);
@@ -21,7 +31,7 @@ function Hero() {
           className="w-full h-full"
         />
       </div>
-
+      <Rings />
       <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-8 sm:mb-12 md:mb-16 lg:mb-[6rem]">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 sm:mb-6 text-white">
           <div className="flex flex-col items-center justify-center gap-2 sm:gap-3">
