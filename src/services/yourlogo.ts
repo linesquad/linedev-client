8680f690ade60ca25a0f15d45107ed4fd13c12a5
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-06-07 13:36:19 +0400

    add yourlogo service and render it in seniordashboard

---
+++ src/services/yourlogo.ts
@@ -0,0 +1,27 @@
+import instance from "../lib/axios";
+
+export const getYourLogo = async () => {
+  const response = await instance.get("/api/yourlogo");
+  return response.data;
+};
+
+export const createYourLogo = async (data: {
+  name: string;
+  image: string;
+}) => {
+  const response = await instance.post("/api/yourlogo", data);
+  return response.data;
+};
+
+export const updateYourLogo = async (id: string, data: {
+  name: string;
+  image: string;
+}) => {
+  const response = await instance.put(`/api/yourlogo/${id}`, data);
+  return response.data;
+};
+
+export const deleteYourLogo = async (id: string) => {
+  const response = await instance.delete(`/api/yourlogo/${id}`);
+  return response.data;
+};
