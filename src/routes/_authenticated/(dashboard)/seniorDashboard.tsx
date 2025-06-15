86d88bf367a64a98942274095370e2610d0909f8
LevaniMesxia23 <levaniko.mesxo.2004@gmail.com>
2025-06-15 15:13:37 +0400

    remove imagekit component from seniordashbaord

--- src/routes/_authenticated/(dashboard)/seniorDashboard.tsx
+++ src/routes/_authenticated/(dashboard)/seniorDashboard.tsx
@@ -5,7 +5,6 @@ import CreateYourLogo from "../../../components/seniordashboard/yourlogo/CreateY
 import { useState } from "react";
 import UpdateYourLogo from "../../../components/seniordashboard/yourlogo/UpdateYourLogo";
 import MainWrapper from "../../../components/MainWrapper";
-import ImageKit from "../../../components/ImageKit";
 
 export const Route = createFileRoute(
   "/_authenticated/(dashboard)/seniorDashboard"
@@ -56,7 +55,6 @@ function SeniorDashboard() {
               setSelectedId={setSelectedId}
             />
             <PricingData />
-            <ImageKit />
           </div>
         </div>
       </div>
